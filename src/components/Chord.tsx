6d1a74c34bd84a94d0ef1baa6a412f0b5fcf628e
jhanca-vm <jhanca-vm@proton.me>
2024-02-19 15:45:18 -0500

    feat: add new song

--- src/components/Chord.tsx
+++ src/components/Chord.tsx
@@ -2,16 +2,19 @@ import type { FunctionComponent } from 'preact'
 
 interface Props {
   value: string
+  symbol?: string
 }
 
-const Chord: FunctionComponent<Props> = ({ value, children }) => {
+const Chord: FunctionComponent<Props> = ({ value, symbol, children }) => {
+  const text = <>{value}{symbol && <small>{symbol}</small>}</>
+
   if (!children) {
-    return <strong class="align-top text-primary-900">{value}</strong>
+    return <strong class="align-top text-primary-900">{text}</strong>
   }
 
   return (
-    <span class="inline-flex flex-col gap-y-0.5 align-bottom">
-      <strong class="text-primary-900">{value}</strong>
+    <span class="inline-grid grid-rows-2 gap-y-0.5 text-left align-bottom">
+      <strong class="text-primary-900">{text}</strong>
       {children}
     </span>
   )
